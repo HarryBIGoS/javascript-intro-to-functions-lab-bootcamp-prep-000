47feace8db626fb2c9f32394d76201f004bee7d0
Harry Bigos <hrbigos@gmail.com>
2018-08-17 19:52:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase())
   return ("I can\'t hear !")
+}
   
   if (string === string.toUpperCase())
   return "YES INDEED!"
