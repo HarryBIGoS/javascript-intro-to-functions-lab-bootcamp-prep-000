996f59de27aa85a735bc4c5e245b5917d0856460
Harry Bigos <hrbigos@gmail.com>
2018-08-17 19:54:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,24 +19,15 @@ function logWhisper(string) {
 
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase())
+  if (string === string.toLowerCase()) {
   return ("I can\'t hear !")
 }
   
-  if (string === string.toUpperCase())
+  if (string === string.toUpperCase()) {
   return "YES INDEED!"
 }
-describe('sayHiToGrandma(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
 
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-})
-
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
